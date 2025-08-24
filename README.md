514eee718e6d773eb5b83a431ee9a013e9907174
Varunan Varathan <varunanvarathan@Varunans-MacBook-Air.local>
2025-08-24 15:07:23 -0400

    Test Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # KaleidoscopeClassicSolver
+
+Description TBD
---
+++ solver/main.cpp
@@ -0,0 +1,11 @@
+// 
+// main.cpp file for Kaleidoscope Classic
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+
+    return 0;
+}
